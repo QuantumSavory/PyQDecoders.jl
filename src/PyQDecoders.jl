01c0812f934a39f2bc4fd94572da12e577566bb6
Stefan Krastanov <github.acc@krastanov.org>
2024-06-19 01:42:43 -0400

    so pip can not keep build dependencies in order

--- src/PyQDecoders.jl
+++ src/PyQDecoders.jl
@@ -1,6 +1,4 @@
 module PyQDecoders
-
-import CondaPkg
 import PythonCall
 
 const sp = PythonCall.pynew()
@@ -11,26 +9,6 @@ const ldpc = PythonCall.pynew()
 const ldpccodes = PythonCall.pynew()
 
 function __init__()
-    println("NUMPY")
-    println("###\n###\n###\n###\n###\n###\n###\n###\n###\n###\n")
-    CondaPkg.add_pip("numpy"; version="<2")
-    println("SCIPY")
-    println("###\n###\n###\n###\n###\n###\n###\n###\n###\n###\n")
-    CondaPkg.add_pip("scipy")
-    println("LDPC")
-    println("###\n###\n###\n###\n###\n###\n###\n###\n###\n###\n")
-    CondaPkg.add_pip("ldpc"; version="@https://github.com/QuantumSavory/ldpc/archive/refs/heads/numpy2.zip")
-    println("PYMATCHING")
-    println("###\n###\n###\n###\n###\n###\n###\n###\n###\n###\n")
-    CondaPkg.add_pip("pymatching")
-    println("STATUS")
-    println("###\n###\n###\n###\n###\n###\n###\n###\n###\n###\n")
-    CondaPkg.status()
-    CondaPkg.resolve(; force=false)
-    CondaPkg.status()
-    CondaPkg.update()
-    CondaPkg.status()
-
     PythonCall.pycopy!(sp, PythonCall.pyimport("scipy"))
     PythonCall.pycopy!(sps, PythonCall.pyimport("scipy.sparse"))
     PythonCall.pycopy!(np, PythonCall.pyimport("numpy"))
