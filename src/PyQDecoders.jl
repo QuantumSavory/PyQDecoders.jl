f1b7c3e8aa39de24f63b2f227272b52d335c23c5
Stefan Krastanov <github.acc@krastanov.org>
2024-06-19 01:09:32 -0400

    maybe the toml file is wrong

--- src/PyQDecoders.jl
+++ src/PyQDecoders.jl
@@ -1,4 +1,6 @@
 module PyQDecoders
+
+import CondaPkg
 import PythonCall
 
 const sp = PythonCall.pynew()
@@ -9,6 +11,11 @@ const ldpc = PythonCall.pynew()
 const ldpccodes = PythonCall.pynew()
 
 function __init__()
+    CondaPkg.add_pip("numpy"; version="<2")
+    CondaPkg.add_pip("scipy")
+    CondaPkg.add_pip("ldpc")
+    CondaPkg.add_pip("pymatching")
+
     PythonCall.pycopy!(sp, PythonCall.pyimport("scipy"))
     PythonCall.pycopy!(sps, PythonCall.pyimport("scipy.sparse"))
     PythonCall.pycopy!(np, PythonCall.pyimport("numpy"))
