38535380488688e8b5b299e92c35df77ed334e93
Stefan Krastanov <stefan@krastanov.org>
2024-09-12 09:19:59 -0400

    simplify jet tests (#13)

--- test/test_jet.jl
+++ test/test_jet.jl
@@ -2,28 +2,11 @@ using Test
 using PyQDecoders
 using JET
 
-using JET: ReportPass, BasicPass, InferenceErrorReport, UncaughtExceptionReport
-
-# Custom report pass that ignores `UncaughtExceptionReport`
-# Too coarse currently, but it serves to ignore the various
-# "may throw" messages for runtime errors we raise on purpose
-# (mostly on malformed user input)
-struct MayThrowIsOk <: ReportPass end
-
-# ignores `UncaughtExceptionReport` analyzed by `JETAnalyzer`
-(::MayThrowIsOk)(::Type{UncaughtExceptionReport}, @nospecialize(_...)) = return
-
-# forward to `BasicPass` for everything else
-function (::MayThrowIsOk)(report_type::Type{<:InferenceErrorReport}, @nospecialize(args...))
-    BasicPass()(report_type, args...)
-end
-
 # imported to be declared as modules filtered out from analysis result
 using CondaPkg, PythonCall
 
 @testset "JET checks" begin
     rep = report_package("PyQDecoders";
-        report_pass=MayThrowIsOk(),
         ignored_modules=(
             AnyFrameModule(CondaPkg),
             AnyFrameModule(PythonCall),
